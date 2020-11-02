3fea0c7d3e1a98466528af77c868933d67847034
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-02 12:03:57 +0100

    job03

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -4,16 +4,56 @@
     <meta charset="utf-8">
     <title></title>
   </head>
+
+
+  <?php
+    $bool = true;
+    $entier = 24;
+    $txt = "Hello Wolrd";
+    $virgul = 24.5;
+
+   ?>
+
+
   <body>
 <!-- =====================HEADER=================== -->
     <header>
-
     </header>
 
 
 <!-- =======================MAIN========================= -->
     <main>
+      <table>
+        <thead>
+          <td>Type</td>
+          <td>Nom</td>
+          <td>Valeur</td>
+        </thead>
 
+        <tr>
+          <td>Booléen</td>
+          <td>bool</td>
+          <td> <?php echo $bool; ?> </td>
+        </tr>
+
+        <tr>
+          <td>Entier</td>
+          <td>entier</td>
+          <td> <?php echo $entier; ?></td>
+        </tr>
+
+        <tr>
+          <td>Chaîne de caractères</td>
+          <td>txt</td>
+          <td><?php echo $txt; ?></td>
+        </tr>
+
+        <tr>
+          <td>nombre à virgule flottante</td>
+          <td>virgul</td>
+          <td><?php echo $virgul ?></td>
+        </tr>
+      </table>
     </main>
 
 
@@ -26,10 +66,3 @@
 </html>
 
 <!-- ============================PHP============================== -->
-<?php
-  $bool = true;
-  $entier = 24;
-  $txt = "Hello Wolrd";
-  $virgul = 24.5;
-
- ?>
