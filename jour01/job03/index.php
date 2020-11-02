af28abc8cea42e7399c663207de60ec582781c59
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-02 11:57:52 +0100

    construciton de la page et des variables

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -1,3 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<!-- =====================HEADER=================== -->
+    <header>
+
+    </header>
+
+
+<!-- =======================MAIN========================= -->
+    <main>
+
+    </main>
+
+
+<!-- ==========================FOOTER========================== -->
+    <footer>
+
+    </footer>
+  </body>
+
+</html>
+
+<!-- ============================PHP============================== -->
 <?php
+  $bool = true;
+  $entier = 24;
+  $txt = "Hello Wolrd";
+  $virgul = 24.5;
 
  ?>
