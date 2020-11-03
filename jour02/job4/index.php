984df996bfa60242934e637118684e8993bb1751
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-03 13:48:20 +0100

    fin job4

--- jour02/job4/index.php
+++ jour02/job4/index.php
@@ -1,4 +1,23 @@
 <?php
+$i = 0;
+while ($i < 100) {
+  if ($i % 3 === 0) {
+    if ($i%5 ===0) {
+      echo "FizzBuzz <br />";
+    }
 
+    else {
+      echo "Fizz <br />";
+    }
+  }
+
+  elseif ($i%5 === 0) {
+    echo "Buzz <br />";
+  }
+  else {
+    echo $i. "<br />";
+  }
+  $i++;
+}
 
  ?>
