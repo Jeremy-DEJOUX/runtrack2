98122d48782659341858d69062776244fe55b9f9
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-03 17:42:19 +0100

    fin job5

---
+++ jour02/job5/index.php
@@ -0,0 +1,13 @@
+<?php
+for ($i=2; $i < 1000 ; $i++) {
+  $prime = true;
+  for ($j=2; $j < $i; $j++) {
+    if ($i % $j == 0) {
+      $prime = false;
+    }
+  }
+  if ($prime) {
+    echo $i . "<br />";
+  }
+}
+?>
