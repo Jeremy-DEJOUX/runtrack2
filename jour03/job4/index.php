c50c64fa17d9795b9682db5c65a6ca88e75ab135
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-04 12:00:37 +0100

    fin job 4 debut job 5

--- jour03/job4/index.php
+++ jour03/job4/index.php
@@ -1,7 +1,9 @@
 <?php
-  $str = "Dans l'espace, personne ne vous entend crier"
-
-
-
- ?>
+  $str = "Dans l'espace, personne ne vous entend crier";
+  $compte = strlen($str);
+  for ($i=0; $i < $compte ; $i++) {
+    echo $str[$i]." C'est le caractère numéro n°". $i ."<br />";
+  }
+  echo "Nomre de carcatères: ". $compte;
+?>
 <!-- Alien -->
