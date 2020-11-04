68e298dac12340802b1a67dbcbd311805f82126e
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-04 20:02:05 +0100

    changement de fonction et Merci Ô grand Guillaume Plantevin

--- jour03/job7/index.php
+++ jour03/job7/index.php
@@ -1,28 +1,27 @@
 <?php
-  function mystrlen($str)
-  {
-  $count = 0;
-  for($i=0; $i<1000000; $i++)
-  {
-  if(@$str[$i] != "")$count++;
-  else break;
-  }
-  return $count;
-  }
-  $str = "bonjour";
-  $compte = mystrlen($str);
-  // $i = 0;
-  // while ($i < $compte) {
-  //   echo $str[$i-1];
-  //   i++;
+  // function mystrlen($str)
+  // {
+  // $count = 0;
+  // for($i=0; $i<1000000; $i++)
+  // {
+  // if(@$str[$i] != "")$count++;
+  // else break;
+  // }
+  // return $count;
   // }
 
-  for ($i=0; $i < $compte ; $i++) {
+  function otherstrlen($str){
+    $compte = 0;
+    while (isset($str[$compte]))
+      $compte++;
+    return $compte;
+  }
+  $str = "Certaines choses changent, et d'autres ne changeront jamais";
+  $ocurence = otherstrlen($str);
+  // $compte = otherstrlen($str);
+  for ($i=0; $i < $ocurence ; $i++) {
     echo $str[$i-1];
   }
-
-
-
 
  ?>
 <!-- Matrix -->
