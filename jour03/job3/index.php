b4cefb3f7fc601e748628208ffd1c210e5639f66
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-04 10:15:46 +0100

    job3 challenge completed

--- jour03/job3/index.php
+++ jour03/job3/index.php
@@ -1,4 +1,16 @@
 <?php
+$str = "I'm sorry Dave I'm afraid I can't do that";
+$voyelles = ["a", "e", "i", "o", "u", "y"];
+
+for ($i=0 ; $i < strlen($str) ; $i++ ) {
+  for ($v=0 ; $v < count($voyelles)  ; $v++ ) {
+    if ($str[$i] == $voyelles[$v]) {
+      echo $str[$i];
+    }
+  }
+
+}
+
 
 
  ?>
