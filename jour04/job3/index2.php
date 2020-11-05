fa4f30448c3dab57cda29c869d9d0a97728a4527
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-05 11:32:23 +0100

    sérieusement

--- jour04/job3/index2.php
+++ jour04/job3/index2.php
@@ -3,7 +3,7 @@
 if (!empty($_POST)) { //SI le forme est pas vide
   echo '<b>Variables</b> :<br />';
     echo '<pre>';
-    print_r($_POST); // afficher les variables var_dump($_GET)
+    print_r($_POST); // afficher les variables var_dump($_POST)
 }
 
  ?>
