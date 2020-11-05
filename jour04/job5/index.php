7814f847637b017017d5a21163322abea667a114
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-05 11:40:46 +0100

    fin job 5 est franchement le dernier Rambo nul nul nul nul nul

---
+++ jour04/job5/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="index2.php" method="post">
+      <label for="username">Username</label>
+      <input type="text" id="username" name="username" value=""/>
+
+      <label for="password">Password</label>
+      <input type="password" id="password" name="password" value=""/>
+
+      <input type="submit"/>
+    </form>
+  </body>
+</html>
