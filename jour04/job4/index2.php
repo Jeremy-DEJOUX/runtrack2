8b0b23b4d20f49e66e06281ab00aab6a6e2ac8c0
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-05 11:35:45 +0100

    fin job 4 début job 5

---
+++ jour04/job4/index2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <td>Prenom</td>
+        <td><?php echo $_POST["prenom"]; ?></td>
+      </tr>
+      <tr>
+        <td>Nom</td>
+        <td><?php echo $_POST["nom"]; ?></td>
+      </tr>
+    </table>
+  </body>
+</html>
