b60984059ef1e17c320645a73762607bc43f4aaf
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-05 11:02:53 +0100

    com

--- jour04/job1/index2.php
+++ jour04/job1/index2.php
@@ -1,9 +1,9 @@
 <?php
 
-if (!empty($_GET)) {
+if (!empty($_GET)) { //SI le forme est pas vide
   echo '<b>Variables</b> :<br />';
     echo '<pre>';
-    print_r($_GET);
+    print_r($_GET); // afficher les variables var_dump($_GET)
 }
 
  ?>
