93e0306e8529d5c7b1b823380a2df30d5191add3
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-06 10:19:30 +0100

    début jour 5 fin job1

--- jour04/job6/index2.php
+++ jour04/job6/index2.php
@@ -1,6 +1,6 @@
 <?php
-  $num = number_format($_GET["nombre"]);
-  // $test = (int)$_GET["nombre"];
+  // $num = number_format($_GET["nombre"]);
+  $num = (int)$_GET["nombre"];
   if ($num%2 == 0) {
     echo "nombre pair";
   }
