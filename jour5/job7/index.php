34eb066f86ec24aa1bff750ae14d7c7546096c96
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-08 19:08:31 +0100

    clean code

--- jour5/job7/index.php
+++ jour5/job7/index.php
@@ -1,31 +1,8 @@
-<!-- <!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
-  </body>
-</html> -->
-
-
-
-
 <?php
 
-  // function mystrlen($str){ //Fonction compte le nombre de caractère
-  //   $compte = 0;
-  //   for ($i=0; isset($str[$i]) ; $i++) {
-  //     $compte++;
-  //   }
-  //   return $compte;
-  // }
 
 
-
-
-  function gras($str){ //Fonction Gras
+  function gras($str){
       $Maj = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
       "T", "U", "V", "W", "X", "Y", "Z"];
       for ($i=0; isset($str[$i]); $i++) {
@@ -47,33 +24,5 @@
       }
     }
 
-
-
-
-  //
-  //
-  // function cesar($str, $option, $decalage){ //Fonction Cesar
-  //   if ($option = "cesar" && ) {
-  //
-  //   }
-  // }
-
-
-
-
-  // function Plateforme($str, $option){ //Fonction Plateforme
-  //   if ($option = "Plateforme") {
-  //     for ($i=0; isset($str[$i]) ; $i++) {
-  //       if ($str[$i] == " " or $str[$i] == ""){
-  //         if ($str[$i-1] == "e" && $str[$i-2] == "m" ) {
-  //           $str[$i] = "_";
-  //         }
-  //       }
-  //     }
-  //   }
-  //   return $str;
-  // }
-
-  // $x = gras("La femme a Plusieurs Amant");
 gras("La femme De Axel");
  ?>
