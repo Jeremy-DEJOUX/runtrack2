af88ce3eafbf012a117512b085d7959d3262d39a
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-11 18:50:14 +0100

    remise à jour du job07

--- jour03/job7/index.php
+++ jour03/job7/index.php
@@ -1,27 +1,29 @@
 <?php
-  // function mystrlen($str)
-  // {
-  // $count = 0;
-  // for($i=0; $i<1000000; $i++)
-  // {
-  // if(@$str[$i] != "")$count++;
-  // else break;
-  // }
-  // return $count;
-  // }
-
-  function otherstrlen($str){
-    $compte = 0;
-    while (isset($str[$compte]))
-      $compte++;
-    return $compte;
-  }
-  $str = "Certaines choses changent, et d'autres ne changeront jamais";
-  $ocurence = otherstrlen($str);
-  // $compte = otherstrlen($str);
-  for ($i=0; $i < $ocurence ; $i++) {
-    echo $str[$i-1];
+  function mystrlen($str){
+    $count = 0;
+    for($i=0; isset($str[$i]); $i++){
+      $count++;
+    }
+    return $count;
   }
 
- ?>
-<!-- Matrix -->
+    $str = "bonjour";
+    for ($i=0; isset($str[$i]); $i++) {
+      echo $str[$i-mystrlen($str)+1];
+    }
+
+
+  // function strlen_copycat($input_string) {
+  //     $i = 0;
+  //     while (isset($input_string[$i])) {
+  //         ++$i;
+  //     }
+  //     return $i;
+  // }
+  // $matrix = "Certaines choses changent, et d'autres ne changeront jamais.";
+  // $length = strlen_copycat($matrix);
+
+  // // Remercions la magie du modulo
+  // for ($i = 0; $i < $length; ++$i)
+  //     echo $matrix[($i + 1) % $length];
+?>
