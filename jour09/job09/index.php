273b59bfe9128c21294e0c8368de82196c3188a7
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-12 15:40:38 +0100

    job09

---
+++ jour09/job09/index.php
@@ -0,0 +1,41 @@
+<?php
+
+$base_de_donnes = mysqli_connect('localhost', 'root', '', 'jour08');
+$querry = mysqli_query($base_de_donnes, "SELECT * FROM `salles` ORDER BY `capacite`");
+$id = mysqli_fetch_all($querry);
+// var_dump($id)
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nom</th>
+                <th>Id_Etage</th>
+                <th>Capacite</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php
+                foreach ($id as $key){
+                    echo "<tr>";
+                    foreach ($key as $value){
+                        echo "<td>$value</td>";
+                    }
+                    echo "</tr>";
+                }
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
