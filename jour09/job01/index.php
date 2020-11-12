0070aaecf298dbbe86c525d29b8611e66dc28359
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-12 14:21:27 +0100

    job01 done

---
+++ jour09/job01/index.php
@@ -0,0 +1,43 @@
+<?php
+
+$base_de_donnes = mysqli_connect('localhost', 'root', '', 'jour08');
+$querry = mysqli_query($base_de_donnes, 'SELECT * FROM `etudiants`');
+$id = mysqli_fetch_all($querry);
+var_dump($id)
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Prenom</th>
+                <th>Nom</th>
+                <th>Naissance</th>
+                <th>Sexe</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php
+                foreach ($id as $key){
+                    echo "<tr>";
+                    foreach ($key as $value){
+                        echo "<td>$value</td>";
+                    }
+                    echo "</tr>";
+                }
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
